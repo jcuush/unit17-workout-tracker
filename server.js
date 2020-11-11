70a0279599e53fd24a0f09b0b52425d207a3b7e3
jcuush <jrcushing@sbcglobal.net>
2020-11-11 15:01:02 -0600

    commented out test function

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello world");
+// });
 
 
 mongoose.connect(
