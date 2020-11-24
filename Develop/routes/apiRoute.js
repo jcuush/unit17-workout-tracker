e8132751daf976a6e67c825d3e779d8b692da543
jcuush <jrcushing@sbcglobal.net>
2020-11-24 17:38:40 -0600

    edited apiRoute.js to add app.put, app.get, app.post

--- Develop/routes/apiRoute.js
+++ Develop/routes/apiRoute.js
@@ -12,4 +12,34 @@ module.exports = function(app){
             res.json(err)
         })
     });
+    app.post("/api/workouts", ({body}, res) => {
+        console.log(body);
+        Workout.create(body)
+          .then(response => {
+            res.json(response);
+          })
+          .catch(err => {
+            res.status(400).json(err);
+          });
+      });
+      app.put("/api/workouts/:id", (req, res) => {
+          Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body}})
+          .then(data => {
+          console.log(data);
+          res.json(data);
+          })
+          .catch(err => {
+          res.status(400).json(err);
+          });
+        });
+        app.get("/api/workouts/range", (req, res) => {
+            Workout.find({})
+            .then(data => {
+              res.json(data);
+            })
+            .catch(err => {
+              res.status(400).json(err);
+            });
+        });
+    
 }
